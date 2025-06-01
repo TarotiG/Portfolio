6047c64b2341d6d54178e6b02054cb9100739e9f
TarotiG <tyronlsg@gmail.com>
2025-06-01 13:56:25 +0200

    Add project files.

---
+++ DevPortfolio/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿namespace DevPortfolio.Models
+{
+    /// <summary>
+    /// Dit is het model voor projecten
+    /// </summary>
+    public class Project
+    {
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string Function { get; set; } = default!;
+        public string Period { get; set; } = default!;
+        public string Technology { get; set; } = default!;
+        public bool PersonalProject { get; set; }
+    }
+}
---
+++ DevPortfolio/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DevPortfolio.Models;
+
+namespace DevPortfolio.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<Project> Projecten { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Projecten = new List<Project>
+            {
+                new Project { Name="Robot Framework", Description="Keyword-based test framework."},
+                new Project { Name="Selenium C#", Description="Code-based test framework dat gebruik maakt van Webdrivers."},
+                new Project { Name="Playwright C#", Description="Code-based test framework dat wordt uitgevoerd met gebruik van de devtools."}
+            };
+
+        }
+    }
+}
