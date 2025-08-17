4adbebccfc443dd8c8d05f0d595eb7733b20a636
TarotiG <tyronlsg@gmail.com>
2025-08-17 16:57:53 +0200

    frontend aanpassingen voor certificaten en projecten section

--- Backend/Services/PDFService.cs
+++ Backend/Services/PDFService.cs
@@ -1,8 +1,14 @@
-﻿namespace Backend.Services
+﻿using PdfiumViewer;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+
+namespace Backend.Services
 {
     public class PDFService
     {
         private readonly IWebHostEnvironment _webHostEnvironment;
+
+        private string BasePath => _webHostEnvironment.IsProduction() ? Path.Combine(_webHostEnvironment.WebRootPath, "pdf-files") : Path.Combine("wwwroot", "pdf-files");
 
         public PDFService(IWebHostEnvironment webHostEnvironment)
         {
@@ -11,8 +17,7 @@
 
         public byte[] GetPDF(string fileName)
         {
-            string filePath;
-            filePath = Path.Combine("wwwroot", "pdf-files", fileName);
+            string filePath = Path.Combine(BasePath, fileName);
 
             if (!File.Exists(filePath))
             {
@@ -22,5 +27,37 @@
             byte[] fileBytes = File.ReadAllBytes(filePath);
             return fileBytes;
         }
+
+        //public Image ConvertPdftoJpg(string fileName)
+        //{
+        //    string filePath = Path.Combine(BasePath, fileName);
+        //    string outputPath = Path.Combine(filePath, "output-jpg");
+
+        //    if (!Directory.Exists(outputPath))
+        //    {
+        //        Directory.CreateDirectory(outputPath);
+        //    }
+
+        //    try
+        //    {
+        //        using (var document = PdfDocument.Load(pdfPath))
+        //        {
+        //            int dpi = 96;
+        //            using (var image = document.Render(1, dpi, dpi, true))
+        //            {
+        //                string jpgPath = Path.Combine(outputPath, $"{fileName}-converted.jpg");
+
+        //                image.Save(jpgPath, ImageFormat.Jpeg);
+        //            }
+        //        }
+
+        //        Console.WriteLine("\nConversie voltooid!");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine($"Er is een fout opgetreden: {ex.Message}");
+        //    }
+        //    return jpgImage;
+        //}
     }
 }
--- Frontend/Pages/Index.cshtml.cs
+++ Frontend/Pages/Index.cshtml.cs
@@ -29,16 +29,6 @@ namespace DevPortfolio.Pages
             Projecten = _projectRepository.GetAllProjectsAsync().Result;
             Certificaten = _certificateRepository.GetAllCertificatesAsync().Result;
 
-            //Projecten = new List<Project>
-            //{
-            //    new Project { Name="Robot Framework", Description="Keyword-based test framework."},
-            //    new Project { Name="Selenium C#", Description="Code-based test framework dat gebruik maakt van Webdrivers."},
-            //    new Project { Name="Playwright C#", Description="Code-based test framework dat wordt uitgevoerd met gebruik van de devtools."},
-            //    new Project { Name="Selenium Python", Description="Code-based test framework dat wordt uitgevoerd met gebruik van de devtools."},
-            //    new Project { Name="Robot Framework - Python", Description="Keyword-based test framework dat code-technische oplossingen bevat."},
-            //    new Project { Name="Cypress", Description="Code-based test framework dat wordt uitgevoerd met gebruik van de devtools."}
-            //};
-
         }
     }
 }
